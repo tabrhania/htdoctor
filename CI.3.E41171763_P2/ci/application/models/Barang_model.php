a184e66e57b63e0ffe23e3b8eef8bef706c15550
tabrhania <44232533+tabrhania@users.noreply.github.com>
2019-03-21 20:32:11 +0700

    layout

---
+++ CI.3.E41171763_P2/ci/application/models/Barang_model.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Barang_model extends CI_Models {
+
+}
---
+++ CI.3.E41171763_P2/ci/application/views/admin/_partial/sidebar.php
@@ -0,0 +1,41 @@
+<ul class="sidebar navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>Master</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <a class="dropdown-item" href="barang.html">Barang</a>
+          <a class="dropdown-item" href="karyawan.html">Karyawan</a>
+          <a class="dropdown-item" href="user.html">User</a>
+          <a class="dropdown-item" href="suplier.html">Suplier</a>
+          
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-fw fa-chart-area"></i>
+          <span>Transaksi</span></a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <a class="dropdown-item" href="penjualan.html">Penjualan</a>
+          <a class="dropdown-item" href="pembelian.html">Pembelian</a>
+          </div>
+
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-fw fa-table"></i>      
+          <span>Laporan</span></a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <a class="dropdown-item" href="harian.html">Harian</a>
+          <a class="dropdown-item" href="bulanan.html">Bulanan</a>
+          <a class="dropdown-item" href="tahunan.html">Tahunan</a>
+          </div>
+      </li>
+    </ul>
