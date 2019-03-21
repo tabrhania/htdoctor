b9389aae94446202b507a38c646e7ae273ef30b7
NauvalPermana <47969857+NauvalPermana@users.noreply.github.com>
2019-03-21 20:39:15 +0700

    Terbaru Noval

---
+++ CI.3.E41171666_P2/framework/application/models/Barang_model.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Barang_model extends CI_Model {
+
+	public function index()
+	{
+		
+	}
+}
---
+++ CI.3.E41171666_P2/framework/application/views/admin/_partials/sidebar.php
@@ -0,0 +1,45 @@
+<!-- Sidebar -->
+<ul class="sidebar navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>Master</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <h6 class="dropdown-header">Sub Menu</h6>
+          <a class="dropdown-item" href="">Barang</a>
+          <a class="dropdown-item" href="">Karyawan</a>
+          <a class="dropdown-item" href="">User</a>
+          <a class="dropdown-item" href="">Supplier</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-fw fa-chart-area"></i>
+          <span>Transaksi</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <h6 class="dropdown-header">Sub Menu</h6>
+            <a class="dropdown-item" href="">Penjualan</a>
+            <a class="dropdown-item" href="">Pembelian</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Laporan</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <h6 class="dropdown-header">Sub Menu</h6>
+          <a class="dropdown-item" href="">Laporan Harian</a>
+          <a class="dropdown-item" href="">Laporan Bulanan</a>
+          <a class="dropdown-item" href="">Laporan Tahunan</a>
+        </div>
+      </li>
+    </ul>
